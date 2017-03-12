70882fb309f613592e0a06595621a392a2130a97
Mikel Matticoli <mikelmm99@gmail.com>
2017-03-11 23:07:22 -0500

    new scrolling method

--- scroll-tests/main.js
+++ scroll-tests/main.js
@@ -7,6 +7,10 @@ var gotoxy = function(x, y, dur=0) {
 }
 
 $('body').fadeTo(0,0);
+gotoxy(0,'50%');
+$("body").stop().animate({
+  backgroundSize: "200%"
+}, 1000);
 
 var snapp, snapping;
 
@@ -31,7 +35,7 @@ $(window).on("scroll", function (args) {
         return;
     }
     
-    var pos = $(window).scrollTop()/100;
+    var pos = $(window).scrollTop()/50;
     
     if(snapp)
         clearTimeout(snapp);
@@ -46,3 +50,45 @@ $(window).on("scroll", function (args) {
     console.log("SCROLL "+pos);
     gotoxy(pos+'%', '50%');
 });
+
+/*var snapp, snapping;
+
+var snap = function() {
+    snapping = true;
+    gotoxy('32%','50%',500);
+    setTimeout(function() {
+        $('body').animate({
+                scrollTop: 3200
+            }, 0);
+        
+        setTimeout(function() {
+            $('body').fadeTo(500,1);
+        },0);
+        snapping=false;
+    },600);
+    
+}
+
+$(window).on("scroll", function (args) {
+    var positionY = -window.pageYOffset/10;
+    $("body").css({
+      'backgroundPositionX': positionY+'px',
+      'backgroundPositionY': '50%'
+    });
+//   document.body.style.backgroundPositionX = "0 -" + positionY + "px";
+//    if(snapping) {
+//        return;
+//    }
+//    
+//    var pos = $(window).scrollTop()/100;
+//    
+//    if(snapp)
+//        clearTimeout(snapp);
+//    if(pos>30 && pos<36) {
+//        snapp = setTimeout(snap,1000);
+//    } else {
+//        console.log("Yee");
+//        setTimeout(function() {
+//            $('body').fadeTo(500,0);   
+//        },0);
+//    }*/
