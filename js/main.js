60b58085afdf70c040643761697ba0101c97bdb7
Mikel Matticoli <mikelmm99@gmail.com>
2017-03-21 13:16:35 -0400

    compressed images

--- js/main.js
+++ js/main.js
@@ -20,7 +20,6 @@ $(document).ready(() => {
     });
     
     $("#event-button").on('click', () => {
-        console.log("test");
         eventsSectionShowing = !eventsSectionShowing;
         if(eventsSectionShowing) {
             $("#events").slideDown();
